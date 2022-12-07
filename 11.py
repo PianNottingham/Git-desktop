8624753b46a1404211d9182eefc83903c09a9f8e
PianNottingham <pian.nottingham@gmail.com>
2022-12-07 10:09:44 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,6 +2,9 @@ print ("sldfksdfjs;fasffsfd")
 print ("sdflkjsfsfasfj;jfsjfsjf;sfsdffdfkd")
 
 
-name = input("Name :")
-print (name)
+#name = input("Name :")
+#print (name)
 #fsdfsdsdsddfssdsds
+
+
+input ("what si your name :")
